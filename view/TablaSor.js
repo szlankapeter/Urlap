ec717aea072233ddf8ea07b7d35f3f24513c6226
szlankapeter <szlankapeter@gmail.com>
2023-11-14 15:27:33 +0100

    Csak név és szül megj

--- view/TablaSor.js
+++ view/TablaSor.js
@@ -1,33 +1,43 @@
-class TablaSor{
+class TablaSor {
 
-    #obj={};
+    #obj = {};
     #index
     #tablaElem
-    constructor(obj, index, tablaElem){
-        this.#obj=obj;
+    constructor(obj, index, tablaElem) {
+        this.#obj = obj;
         this.#index = index;
         this.#tablaElem = tablaElem;
         this.#sor();
         this.sorElem = this.#tablaElem.children("tr:last-child");
         this.TorolGomb = this.sorElem.children("td").children(".remove")
-        this.TorolGomb.on('click', ()=>{
+        this.TorolGomb.on('click', () => {
             this.#esemenyTrigger("törlés");
         })
     }
 
-    #sor(){
-        let txt = "<tr>";
+    #sor() {
+
+        //Összes adattal
+        /* let txt = "<tr>";
          for (const key in this.#obj) {
              txt += `<td>${this.#obj[key]}</td>`;
          }
          txt += `<td><button type="button" class="remove">TÖRÖL</button></td></tr>`;
          txt += "</tr>";
-         this.#tablaElem.append(txt);
-     }
+         this.#tablaElem.append(txt); */
 
-     #esemenyTrigger(esemenyneve){
-        const esemenyem = new CustomEvent(esemenyneve,{detail:this.#obj['id']});
+         //Csak név és szül
+        let txt = "<tr>";
+        txt += `<td>${this.#obj['nev']}</td>`;
+        txt += `<td>${this.#obj['szul']}</td>`;
+        txt += `<td><button type="button" class="remove">TÖRÖL</button></td></tr>`;
+        txt += "</tr>";
+        this.#tablaElem.append(txt);
+    }
+
+    #esemenyTrigger(esemenyneve) {
+        const esemenyem = new CustomEvent(esemenyneve, { detail: this.#obj['id'] });
         window.dispatchEvent(esemenyem);
     }
 
-}export default TablaSor
+} export default TablaSor
